adf188c223e0d2f65d89f077f4d8706581f03677
Armand Roussel <a.roussel@beapp.fr>
2024-01-12 11:34:33 +0100

    initial commit

---
+++ InStreamSDK.xcframework/ios-arm64_x86_64-simulator/InStreamSDK.framework/Headers/InStreamSDK.h
@@ -0,0 +1,18 @@
+//
+//  InStreamSDK.h
+//  InStreamSDK
+//
+//  Created by ARoussel on 03/01/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InStreamSDK.
+FOUNDATION_EXPORT double InStreamSDKVersionNumber;
+
+//! Project version string for InStreamSDK.
+FOUNDATION_EXPORT const unsigned char InStreamSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InStreamSDK/PublicHeader.h>
+
+
